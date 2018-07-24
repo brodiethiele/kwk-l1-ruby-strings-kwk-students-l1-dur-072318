3bede8685946f7b8ffd371844c43f7bc7ee4f63e
brodiethiele <brodie.thiele@gmail.com>
2018-07-24 13:13:12 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -2,14 +2,18 @@
 
 # Try starting out with puts'ing a string.
 guest_name=gets.strip
-puts "Dear #{guest_name},"
 party_name=gets.strip
-puts "You are cordially invited to the #{party_name}"
 date=gets.strip
-puts "on #{date}"
 time=gets.strip
-puts "at #{time}."
 host_name=gets.strip
+puts "Dear #{guest_name},"
+
+puts "You are cordially invited to the #{party_name}"
+
+puts "on #{date}"
+
+puts "at #{time}."
+
 puts "Please RSVP no later than October 30.
  
 Sincerely,
